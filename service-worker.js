4758af25bb7b6fd2dabd54a79bc0c3ca3be48b46
Eugene <evgnkuchma@gmail.com>
2020-04-24 00:30:18 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.acb2eb32aae0492f6c2f4f600525bd72.js"
 );
 
 self.addEventListener('message', (event) => {
